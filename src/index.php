f0664adf833da1b0f21bae2d9ce4aa7b9e4b278e
Nabrih <ahmadnabrih2000@gmail.com>
2023-04-13 16:18:22 +0700

    init

---
+++ src/index.php
@@ -0,0 +1,17 @@
+<?php 
+phpinfo();
+
+// $ch = curl_init("http://host.docker.internal:85/");
+// $fp = fopen("example_homepage.txt", "w");
+
+// curl_setopt($ch, CURLOPT_FILE, $fp);
+// curl_setopt($ch, CURLOPT_HEADER, 0);
+
+// curl_exec($ch);
+// if(curl_error($ch)) {
+//     fwrite($fp, curl_error($ch));
+// }
+// curl_close($ch);
+// fclose($fp);
+
+ ?>
